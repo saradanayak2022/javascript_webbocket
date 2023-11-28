706b474b3204d753999658f43c2588634c7a45be
saradanayak2022 <152000105+saradanayak2022@users.noreply.github.com>
2023-11-28 10:22:33 +0000

    introduction file saved

---
+++ 01-basics/test.js
@@ -0,0 +1,7 @@
+console.log("hellooworld")
+/*
+1.javascript is a highlevel scripting language to use on devloping web and mobile application efficiently
+2.We used javascript onclient side as well as server side
+3.in javascript we do logical implementation in web pages
+4.in today era Esb version of javascript ,as it is latest one
+*/
